f2c29984625b9cdc75a0232920d5ccbbe44d49b4
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2026-09-17 16:29:07 +0530

    refactor: remove quick wins audio track sequences and update scene documentation

--- src/audio/AudioTrack.tsx
+++ src/audio/AudioTrack.tsx
@@ -4,20 +4,7 @@ import { Audio, Sequence, staticFile } from 'remotion';
 export const AudioTrack: React.FC = () => {
   return (
     <>
-      {/* Note: Scenes 1, 2, 3, 4, and 5 (0 - 1560f) have self-contained procedurally synchronized audio cues */}
-
-      {/* 6. Quick Wins Scene Chime (Starts at frame 1560) */}
-      <Sequence from={1560} durationInFrames={60}>
-        <Audio src={staticFile('audio/whoosh.wav')} volume={0.5} />
-      </Sequence>
-      <Sequence from={1630} durationInFrames={90}>
-        <Audio src={staticFile('audio/chime.wav')} volume={0.7} />
-      </Sequence>
-      {[1680, 1720, 1760].map((f, i) => (
-        <Sequence key={`win-${i}`} from={f} durationInFrames={15}>
-          <Audio src={staticFile('audio/click.wav')} volume={0.4} />
-        </Sequence>
-      ))}
+      {/* Note: Scenes 1 through 6 (0 - 1920f) have self-contained procedurally synchronized audio cues */}
 
       {/* 7. Product Montage rapid cuts (Starts at frame 1920) */}
       <Sequence from={1920} durationInFrames={60}>
